1ca4411f09125e67f59ddd8acac37ee3ad2b05d3
Devansh Seth <dev.devanshseth.engg0072@gmail.com>
2026-03-25 17:48:30 +0530

    Added specific imports to Author, removed whole import

--- src/main/java/com/bookinventory/model/Author.java
+++ src/main/java/com/bookinventory/model/Author.java
@@ -1,11 +1,15 @@
 package com.bookinventory.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "author")
 public class Author {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "authorID")
