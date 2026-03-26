6a749dc8d522708031b397bc7f04b6b63c0303d3
sourav-pandey-dev <programmerxsourav@gmail.com>
2026-03-26 23:10:21 +0530

    Created the inventory and reviewer entities

---
+++ src/main/java/com/bookinventory/model/Inventory.java
@@ -0,0 +1,78 @@
+package com.bookinventory.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "inventory")
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "InventoryID")
+    private Integer inventoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "ISBN", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "Ranks", nullable = false)
+    private BookCondition bookCondition;
+
+    @Column(name = "Purchased")
+    private Boolean purchased;
+
+    public Inventory() {
+    }
+
+    public Inventory(
+            Integer inventoryId,
+            Book book,
+            BookCondition bookCondition,
+            Boolean purchased
+    ) {
+        this.inventoryId = inventoryId;
+        this.book = book;
+        this.bookCondition = bookCondition;
+        this.purchased = purchased;
+    }
+
+    public Integer getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(Integer inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public BookCondition getBookCondition() {
+        return bookCondition;
+    }
+
+    public void setBookCondition(BookCondition bookCondition) {
+        this.bookCondition = bookCondition;
+    }
+
+    public Boolean getPurchased() {
+        return purchased;
+    }
+
+    public void setPurchased(Boolean purchased) {
+        this.purchased = purchased;
+    }
+}
