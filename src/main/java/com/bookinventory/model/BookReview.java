428d4acc994e8763843cbf02f10942c8e0e0b464
sourav-pandey-dev <128667235+sourav-pandey-dev@users.noreply.github.com>
2026-03-29 14:12:27 +0530

    fix: resolve JPA IdClass mapping errors for composite keys in bridge … (#24)
    * fix: resolve JPA IdClass mapping errors for composite keys in bridge tables
    
    * style: replace wildcard imports with specific jakarta.persistence imports

--- src/main/java/com/bookinventory/model/BookReview.java
+++ src/main/java/com/bookinventory/model/BookReview.java
@@ -12,73 +12,74 @@ import jakarta.persistence.Table;
 @Table(name = "bookreview")
 @IdClass(BookReviewId.class)
 public class BookReview {
-	@Id
-	@ManyToOne
-	@JoinColumn(
-		name = "ISBN", 
-		referencedColumnName = "ISBN", 
-		columnDefinition = "CHAR(13)"
-	)
-	private Book book;
-	
-	@Id
+
+    @Id
     @ManyToOne
     @JoinColumn(
-    	name = "ReviewerID", 
-    	referencedColumnName = "ReviewerID"
+            name = "ISBN", 
+            referencedColumnName = "ISBN", 
+            columnDefinition = "CHAR(13)"
+    )
+    private Book book;
+    
+    @Id
+    @ManyToOne
+    @JoinColumn(
+            name = "ReviewerID", 
+            referencedColumnName = "ReviewerID"
     )
     private Reviewer reviewer;
 
-	@Column(name = "Rating")
-	private Integer rating;
+    @Column(name = "Rating")
+    private Integer rating;
 
-	@Column(name = "Comments", length = 255)
-	private String comments;
+    @Column(name = "Comments", length = 255)
+    private String comments;
 
-	public BookReview() {
-	}
+    public BookReview() {
+    }
 
-	public BookReview(
-		Book book, 
-		Reviewer reviewer, 
-		Integer rating, 
-		String comments
-	) {
-		this.book = book;
-		this.reviewer = reviewer;
-		this.rating = rating;
-		this.comments = comments;
-	}
+    public BookReview(
+            Book book, 
+            Reviewer reviewer, 
+            Integer rating, 
+            String comments
+    ) {
+        this.book = book;
+        this.reviewer = reviewer;
+        this.rating = rating;
+        this.comments = comments;
+    }
 
-	public Book getBook() {
-		return book;
-	}
+    public Book getBook() {
+        return book;
+    }
 
-	public void setBook(Book book) {
-		this.book = book;
-	}
+    public void setBook(Book book) {
+        this.book = book;
+    }
 
-	public Reviewer getReviewer() {
-		return reviewer;
-	}
+    public Reviewer getReviewer() {
+        return reviewer;
+    }
 
-	public void setReviewerID(Reviewer reviewer) {
-		this.reviewer = reviewer;
-	}
+    public void setReviewer(Reviewer reviewer) {
+        this.reviewer = reviewer;
+    }
 
-	public Integer getRating() {
-		return rating;
-	}
+    public Integer getRating() {
+        return rating;
+    }
 
-	public void setRating(Integer rating) {
-		this.rating = rating;
-	}
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
 
-	public String getComments() {
-		return comments;
-	}
+    public String getComments() {
+        return comments;
+    }
 
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
 }
