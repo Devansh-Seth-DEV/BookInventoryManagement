ab338db7edb77483022ac901db10b7a1a7dd4693
sourav-pandey-dev <programmerxsourav@gmail.com>
2026-03-29 13:00:00 +0530

    style: replace wildcard imports with specific jakarta.persistence imports

--- src/main/java/com/bookinventory/model/BookReview.java
+++ src/main/java/com/bookinventory/model/BookReview.java
@@ -1,6 +1,12 @@
 package com.bookinventory.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "bookreview")
